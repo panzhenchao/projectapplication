193fa7617e9f2d44516061e9107d5716ccc956f1
panzhenchao <penzhenchao@gmail.com>
2019-07-25 14:26:22 +0800

    creat project

---
+++ main/java/com/it/projectapplication/ProjectapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.it.projectapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectapplicationApplication.class, args);
+    }
+
+}
